f4c3f981029d104e3609af8cf8eb255a2ef2f6ac
adipatiarya <aryaadipati2@gmail.com>
2020-05-20 18:48:13 +0700

    initial commit

---
+++ src/config/api.js
@@ -0,0 +1,10 @@
+export const API = 'http://localhost';
+
+export const CONSUMER_KEY = 'ck_6865fdb37b0f4d7f63ea7b48664455cc26425a84';
+export const CONSUMER_SECRET = 'cs_dd26a34b2a1ffed1b46550242a79d8a2bddb34cf';
+
+export default {
+  API_ENDPOINT: API,
+  CONSUMER_KEY,
+  CONSUMER_SECRET
+};
